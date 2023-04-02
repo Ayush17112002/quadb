d1070120bf426057d2fce7df1da7bdc6dfba2b44
Ayush <aayushgupta023@gmail.com>
2023-04-02 15:25:53 +0530

    selector added

--- script.js
+++ script.js
@@ -43,6 +43,14 @@ tableElement = tableElement[0];
     }
     const data = await response.json();
     let sno = 1;
+    tableElement.innerHTML = `<tr><th scope="col">S.No.</th>
+    <th scope="col">NAME</th>
+    <th scope="col">BUY</th>
+    <th scope="col">SELL</th>
+    <th scope="col">LAST PRICE</th>
+    <th scope="col">VOLUME</th>
+    <th scope="col">BASE UNIT</th>
+  </tr>`;
     data.map((row) => {
       let rowEle = document.createElement("tr");
       rowEle.setAttribute("scope", "row");
@@ -50,7 +58,7 @@ tableElement = tableElement[0];
         row["buy"]
       }</td><td>₹ ${row["sell"]}</td><td>₹ ${row["last"]}</td><td>${
         row["volume"]
-      }</td><td>${row["base_unit"]}</td>`;
+      }</td><td>${row["baseunit"]}</td>`;
       rowEle.innerHTML = text;
       tableElement.appendChild(rowEle);
     });
@@ -58,3 +66,39 @@ tableElement = tableElement[0];
     console.log(error.message);
   }
 })();
+let crypto = document.getElementById("crypto");
+let cryptoDisplay = document.getElementById("crypto-selected");
+async function cryptoHandler(e) {
+  const val = crypto.value;
+  cryptoDisplay.innerHTML = `<b>BUY ${val.toUpperCase()}</b>`;
+  try {
+    const response = await fetch(`https://kartikey.me?baseunit=${val}`);
+    if (response.status === 400) {
+      throw new Error("Could not fetch");
+    }
+    const data = await response.json();
+    console.log(data);
+    let sno = 1;
+    tableElement.innerHTML = `<tr><th scope="col">S.No.</th>
+    <th scope="col">NAME</th>
+    <th scope="col">BUY</th>
+    <th scope="col">SELL</th>
+    <th scope="col">LAST PRICE</th>
+    <th scope="col">VOLUME</th>
+    <th scope="col">BASE UNIT</th>
+  </tr>`;
+    data.map((row) => {
+      let rowEle = document.createElement("tr");
+      rowEle.setAttribute("scope", "row");
+      let text = `<td>${sno++}</td><td>${row["name"]}</td><td>₹ ${
+        row["buy"]
+      }</td><td>₹ ${row["sell"]}</td><td>₹ ${row["last"]}</td><td>${
+        row["volume"]
+      }</td><td>${row["baseunit"]}</td>`;
+      rowEle.innerHTML = text;
+      tableElement.appendChild(rowEle);
+    });
+  } catch (error) {
+    alert(error.message);
+  }
+}
